9ef743f6ac941e0ee8086e1bed2267649cdfb1c2
Matthew Waddell <matt.waddell@carvesystems.com>
2025-06-11 09:48:17 -0700

    Add musl-compat-headers... cause musl

---
+++ package/musl-compat-headers/musl-compat-headers.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# musl-compat-headers
+#
+################################################################################
+
+# https://github.com/buildroot/buildroot/blob/2024.11.x/package/musl-compat-headers/musl-compat-headers.mk
+
+MUSL_COMPAT_HEADERS_SOURCE = queue.h
+MUSL_COMPAT_HEADERS_SITE = https://raw.githubusercontent.com/NetBSD/src/03be82a6b173b3c62116b7a186067fed3004dd44/sys/sys
+
+MUSL_COMPAT_HEADERS_LICENSE = BSD-3-Clause, Public Domain or CC0
+MUSL_COMPAT_HEADERS_LICENSE_FILES = queue.h cdefs.h
+
+MUSL_COMPAT_HEADERS_ADD_TOOLCHAIN_DEPENDENCY = NO
+
+# Only installs headers
+MUSL_COMPAT_HEADERS_INSTALL_TARGET = NO
+MUSL_COMPAT_HEADERS_INSTALL_STAGING = YES
+
+# Copying both headers so legal-info finds them (they are _LICENSE_FILES)
+define MUSL_COMPAT_HEADERS_EXTRACT_CMDS
+	$(INSTALL) -m 0644 -D $(DL_DIR)/queue.h $(@D)/queue.h
+	$(INSTALL) -m 0644 -D $(PKGDIR)/cdefs.h $(@D)/cdefs.h
+endef
+
+define MUSL_COMPAT_HEADERS_INSTALL_STAGING_CMDS
+	$(INSTALL) -D -m 0644 $(@D)/queue.h \
+		$(STAGING_DIR)/usr/include/sys/queue.h
+	$(INSTALL) -D -m 0644 $(@D)/cdefs.h \
+		$(STAGING_DIR)/usr/include/sys/cdefs.h
+endef
+
+$(eval $(generic-package))
--- package/musl/musl.mk
+++ package/musl/musl.mk
@@ -13,6 +13,13 @@ MUSL_LICENSE_FILES = COPYRIGHT
 # cross-compiler and the kernel headers
 MUSL_DEPENDENCIES = host-gcc-initial linux-headers
 
+# musl does not provide an implementation for sys/queue.h or sys/cdefs.h.
+# So, add the musl-compat-headers package that will install those files,
+# into the staging directory:
+#   sys/queue.h:  header from NetBSD
+#   sys/cdefs.h:  minimalist header bundled in Buildroot
+MUSL_DEPENDENCIES += musl-compat-headers
+
 # musl is part of the toolchain so disable the toolchain dependency
 MUSL_ADD_TOOLCHAIN_DEPENDENCY = NO
 
