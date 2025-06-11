465b54a30fbe57ce45c1e7657d6f96cf9c743882
Matthew Waddell <matt.waddell@carvesystems.com>
2025-06-11 12:54:17 -0700

    Bump minicom version

--- package/minicom/minicom.mk
+++ package/minicom/minicom.mk
@@ -4,17 +4,34 @@
 #
 ################################################################################
 
-MINICOM_VERSION = 2.7
-MINICOM_SITE = http://sources.buildroot.net
-MINICOM_LICENSE = GPLv2+
+MINICOM_VERSION = 19ab49422f3431102c31fea01549121385113f80
+MINICOM_SITE = https://salsa.debian.org/minicom-team/minicom.git
+MINICOM_SITE_METHOD = git
+MINICOM_LICENSE = GPL-2.0+
 MINICOM_LICENSE_FILES = COPYING
+MINICOM_AUTORECONF = YES
 
 # pkg-config is only used to check for liblockdev, which we don't have
-# in BR, so instead of adding host-pkgconf as a dependency, simply
-# make sure the host version isn't used so we don't end up with problems
-# if people have liblockdev1-dev installed
+# in BR, so instead of adding host-pkgconf as a dependency, simply make
+# sure the host version isn't used so we don't end up with problems if
+# people have liblockdev1-dev installed
 MINICOM_CONF_ENV = PKG_CONFIG=/bin/false
 
-MINICOM_DEPENDENCIES = ncurses $(if $(BR2_ENABLE_LOCALE),,libiconv)
+
+MINICOM_DEPENDENCIES = ncurses $(if $(BR2_ENABLE_LOCALE),,libiconv) \
+	$(TARGET_NLS_DEPENDENCIES)
+# add host-gettext for AM_ICONV macro
+MINICOM_DEPENDENCIES += host-gettext
+
+ifeq ($(BR2_PACKAGE_GETTEXT),y)
+MINICOM_CONF_OPTS += --with-libintl-prefix=$(STAGING_DIR)/usr
+MINICOM_CONF_DEPENDENCIES += gettext libintl
+endif
+
+# Autoreconf requires an existing m4 directory
+define MINICOM_MKDIR_M4
+	mkdir -p $(@D)/m4
+endef
+MINICOM_POST_PATCH_HOOKS += MINICOM_MKDIR_M4
 
 $(eval $(autotools-package))
