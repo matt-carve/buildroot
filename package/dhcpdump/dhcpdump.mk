4d12b7b8cfb77512794a482a544b4a31a99b110f
Matthew Waddell <matt.waddell@carvesystems.com>
2025-06-11 10:54:49 -0700

    Fix dhcpdump + musl

--- package/dhcpdump/dhcpdump.mk
+++ package/dhcpdump/dhcpdump.mk
@@ -15,8 +15,11 @@ ifeq ($(BR2_PREFER_STATIC_LIB),y)
 DHCPDUMP_LIBS += $(shell $(STAGING_DIR)/usr/bin/pcap-config --static --additional-libs)
 endif
 
+# Use gnu-compat names for udp struct members (when using musl)
+DHCPDUMP_CFLAGS = -D_GNU_SOURCE
+
 define DHCPDUMP_BUILD_CMDS
-	$(MAKE) -C $(@D) CC="$(TARGET_CC) $(TARGET_CFLAGS)" LIBS="$(DHCPDUMP_LIBS)"
+	$(MAKE) -C $(@D) CC="$(TARGET_CC) $(TARGET_CFLAGS) $(DHCPDUMP_CFLAGS)" LIBS="$(DHCPDUMP_LIBS)"
 endef
 
 define DHCPDUMP_INSTALL_TARGET_CMDS
